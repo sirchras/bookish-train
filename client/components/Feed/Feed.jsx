1abdb759c6357a28f12478fc223fa1606c5d52d7
sirchras <cjlynch@hotmail.co.nz>
2021-03-25 13:58:04 +1300

    add posts slice

--- client/components/Feed/Feed.jsx
+++ client/components/Feed/Feed.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { fetchPosts } from './postsSlice'
 
 function Feed () {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    console.log('hello world!')
+    dispatch(fetchPosts())
+  }, [])
+
   return (
     <h1>hello world!</h1>
   )
--- client/store.js
+++ client/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import postsSlice from './components/Feed/postsSlice'
+
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    posts: postsSlice
+  }
 })
 
 export default store
