4c61618b9ffe8dd6ea49efc0a84528bf46c1e46c
sirchras <cjlynch@hotmail.co.nz>
2021-03-29 18:22:14 +1300

    add new post component

---
+++ client/components/NewPost.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NewPost () {
+  return (
+    <form>
+      <textarea placeholder={'What\'s up?'}/>
+      <div>
+        <button>Post</button>
+      </div>
+    </form>
+  )
+}
+
+export default NewPost
