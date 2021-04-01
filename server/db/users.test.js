f7bba35589e55b6735384e9a0bd4eab33e844d63
sirchras <cjlynch@hotmail.co.nz>
2021-04-01 16:11:10 +1300

    add tests for user queries

--- server/db/users.test.js
+++ server/db/users.test.js
@@ -13,13 +13,46 @@ beforeEach(() => {
 })
 
 describe('userExists', () => {
-  it.todo('returns whether user exists')
+  it('returns true if the user exists', () => {
+    return db.userExists('chris', testDb)
+      .then(exists => {
+        expect(exists).toBeTruthy()
+        return null
+      })
+  })
+
+  it('returns false if the user doesn\'t exist', () => {
+    return db.userExists('pete', testDb)
+      .then(exists => {
+        expect(exists).toBeFalsy()
+        return null
+      })
+  })
 })
 
 describe('getUserByName', () => {
-  it.todo('returns the user with the matching name')
+  it('returns the user with the matching name', () => {
+    return db.getUserByName('chris', testDb)
+      .then(user => {
+        expect(user).toHaveProperty('id')
+        expect(user.name).toMatch('chris')
+        expect(user).toHaveProperty('hash')
+        return null
+      })
+  })
 })
 
 describe('createUser', () => {
-  it.todo('returns the id of the new user')
+  it('returns the id of the new user', () => {
+    return db.createUser('pete', testDb)
+      .then(id => {
+        expect(id).toBe(3)
+        return null
+      })
+  })
+
+  it('throws an error if the user already exists', () => {
+    return expect(db.createUser('chris', testDb))
+      .rejects.toMatch('user already exists')
+  })
 })
